006748472e5c5a9e6acd9874796111a3a11563ef
yellowfully <richenyou@gmail.com>
2021-10-05 11:20:47 +0800

    comments added

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,38 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    plugins: [new ESLintPlugin({extensions:['.js','.jsx']})],
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+  // Eslint官網新的用法
+  plugins: [new ESLintPlugin({ extensions: ['.js', '.jsx'] })],
+  // 告訴webpack，所有js的code都從這裡丟給webpack
+  entry: './src/index.js',
+  // 告訴webpack，他處理好的檔案要丟去哪邊
+  // 配合index.html要把這個丟出來的檔案載入
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  // 裡面放loader
+  module: {
+    rules: [
+      {
+        // 過濾的條件是.js或是.jsx，這是regex語法
+        test: /\.(js|jsx)$/,
+        // 不需要過濾的是node_modules，這也是regex語法
+        exclude: /node_modules/,
+        use: 'babel-loader',
       },
-    module:{
-        rules:[
-            {
-                test:/\.(js|jsx)$/,
-                exclude:/node_modules/,
-                use:'babel-loader'
-            }
-        ]
+    ],
+  },
+  // 用來讓webpack處理import和export的檔案條件
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  // 用來設定development環境下，用來放預覽網頁的server
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
     },
-    resolve:{
-        extensions:['.js','.jsx']
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
-        },
-        compress: true,
-        port: 8080,
-    }
+    compress: true,
+    port: 8080,
+  },
 };
